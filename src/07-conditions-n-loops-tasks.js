45884e82f48736ee3c5c14ddd314b1e743eabc77
Den54321 <dennbuika@gmail.com>
2023-06-05 10:50:37 +0300

    feat: add solution for 07

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -28,12 +28,11 @@
  *
  */
 function getFizzBuzz(num) {
-  if(num == 0) return num;
-   if(num % 3 == 0 && num % 5 == 0)  return 'FizzBuzz';
-   if(num % 3 == 0) return 'Fizz';
-   if(num % 5 == 0) return 'Buzz';
-   return num;
-
+  if (num === 0) return num;
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
+  return num;
 }
 
 
@@ -48,8 +47,16 @@ function getFizzBuzz(num) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  let res;
+  if (n === 0) return 1;
+  for (let i = 1; i <= n; i += 1) {
+    if (i === 1) res = i;
+    else {
+      res *= i;
+    }
+  }
+  return res;
 }
 
 
@@ -65,8 +72,12 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let res = n1;
+  for (let i = n1 + 1; i <= n2; i += 1) {
+    res += i;
+  }
+  return res;
 }
 
 
@@ -85,8 +96,11 @@ function getSumBetweenNumbers(/* n1, n2 */) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  if (a + b <= c) return false;
+  if (b + c <= a) return false;
+  if (a + c <= b) return false;
+  return true;
 }
 
 
@@ -122,8 +136,19 @@ function isTriangle(/* a, b, c */) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const pos1 = [];
+  for (let x = rect1.left; x <= rect1.left + rect1.width; x += 1) {
+    for (let y = rect1.top; y <= rect1.top + rect1.height; y += 1) {
+      pos1.push(`${x}${y}`);
+    }
+  }
+  for (let x = rect2.left; x <= rect2.left + rect2.width; x += 1) {
+    for (let y = rect2.top; y <= rect2.top + rect2.height; y += 1) {
+      if (pos1.indexOf(`${x}${y}`, 0) !== -1) return true;
+    }
+  }
+  return false;
 }
 
 
@@ -153,8 +178,10 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const pos = (circle.center.x - point.x) ** 2 + (circle.center.y - point.y) ** 2;
+  if (pos < circle.radius ** 2) return true;
+  return false;
 }
 
 
@@ -169,8 +196,12 @@ function isInsideCircle(/* circle, point */) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  for (let i = 0; i < str.length; i += 1) {
+    // eslint-disable-next-line max-len
+    if (str.slice(0, i).indexOf(str[i]) === -1 && str.slice(i + 1).indexOf(str[i]) === -1) return str[i];
+  }
+  return null;
 }
 
 
@@ -196,8 +227,9 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  if (a < b) return `${isStartIncluded ? '[' : '('}${a}, ${b}${isEndIncluded ? ']' : ')'}`;
+  return `${isStartIncluded ? '[' : '('}${b}, ${a}${isEndIncluded ? ']' : ')'}`;
 }
 
 
